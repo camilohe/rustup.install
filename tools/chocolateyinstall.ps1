3104e51958b9c9a747a4314ec4b4ae482a8c1094
camilohe <camilohe@gmail.com>
2019-09-27 16:26:05 -0400

    add workaround for choco-verifier on win2012r2x64

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -19,6 +19,8 @@ $packageArgs = @{
     checksumType64 = 'sha256' 
 
     silentArgs     = '-v -y' # it seems we need '-v -y' starting with 1.9.0 to get rustup copied to the .cargo\bin folder.
+
+    validExitCodes = @(0, -1073741515) # workaround for choco-verifier on win2012r2x64 (Windows Server 2012 R2 x64)
 }
 
 Install-ChocolateyPackage @packageArgs 
